6a545e10b5a89dee4a024a590af2e58acea17c7b
Andrii Mykhailenko <finkons87@gmail.com>
2021-11-29 21:52:30 +0200

    добавляет свг

--- js/modal.js
+++ js/modal.js
@@ -10,3 +10,17 @@ const refs = {
     function toggleModal() {
         refs.backdrop.classList.toggle("is-hidden");
     }
+
+    (() => {
+        document
+        .querySelector('.js-speaker-form')
+        .addEventListener('submit', e => {
+            e.preventDefault();
+
+            new FormData(e.currentTarget).forEach((value, name) =>
+            console.log(`${name}: ${value}`),
+            );
+
+            e.currentTarget.reset();
+        });
+    })();
